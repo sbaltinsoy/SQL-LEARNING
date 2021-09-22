c7cd315ecb44feffdd0f0d60582743a1126ffd30
sbaltinsoy <serhatburakaltinsoy@gmail.com>
2021-09-22 12:51:18 +0300

    Intersect Except

---
+++ UnionExceptInstersect.sql
@@ -0,0 +1,6 @@
+Select OgrAd, OgrSoyad From TblOgrenci
+--Union		-- Tekrar eden verileri 1 kez  yazmak sartiyla 2 tabloyu birlestirir
+--Union All -- Ne var ne yok her seyi birlestiriyor veri tekrari yapar
+--Intersect	-- Sadece Ortak olan yani ayni verileri ekrana yazar
+Except -- Sadece farkli olan verileri ekrana yazar
+Select Adi,Soyadi From Ogrenci2
